3a4f4e53a135ea96d16939885b8960ff533b6b75
rohannikale <rohanpadit745@gmail.com>
2025-03-26 20:56:43 +0530

    error solve

--- server.js
+++ server.js
@@ -16,13 +16,14 @@ const routes = [
 ];
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to database
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({origin:"*"
+}));
 app.use(express.json());
 
 // Register routes
